6f6eba3d4dc27e37280253f391f9f9484771eb7a
virajnemane31 <viraj.nemane@genmills.com>
2022-08-01 10:59:08 +0530

    fix script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hi from `hostname`  at `date`..!" >> demo.txt
-sleep5
+sleep 5
 echo "Hi from `hostname`  at `date`..!" >> demo.txt
-sleep5
+sleep 5
 echo "Hi from `hostname`  at `date`..!" >> demo.txt

d5342f2352334557ca5b1f3e1dde259a715be52a
virajnemane31 <viraj.nemane@genmills.com>
2022-08-01 10:46:43 +0530

    new file

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hi from `hostname`  at `date`..!" >> demo.txt
+sleep5
+echo "Hi from `hostname`  at `date`..!" >> demo.txt
+sleep5
+echo "Hi from `hostname`  at `date`..!" >> demo.txt

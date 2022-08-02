e229bc8a57b8d571407aa9fb0e5b926a252e705b
virajnemane31 <viraj.nemane@genmills.com>
2022-08-02 15:08:21 +0530

    gcp disk terraform

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  project     = "kulbhushanmayer"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+data "google_compute_disk" "demo31" {
+    name = "demo31"
+    project = "kulbhushanmayer"
+}
+
+output "demo" {
+    value = data.google_compute_disk.demo31
+}

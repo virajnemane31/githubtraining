467da31950a2572f4593182bb82332e31b5f8c21
virajnemane31 <viraj.nemane@genmills.com>
2022-08-02 15:56:58 +0530

    make disk

--- main.tf
+++ main.tf
@@ -9,6 +9,17 @@ data "google_compute_disk" "demo31" {
     project = "kulbhushanmayer"
 }
 
+resource "google_compute_disk" "demo31" {
+  name  = "demo31"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-9-stretch-v20200805"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+}
+
 output "demo31" {
     value = data.google_compute_disk.demo31
 }
